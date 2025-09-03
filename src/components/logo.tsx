2ad04f33659c9bb9eaca31fef08969f50554b010
Ichrak Nour <ichraknour22@gmail.com>
2025-09-03 04:03:09 +0000

    this is the link of logo https://res.cloudinary.com/drdebnl11/image/uplo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo() {
   return (
     <Image
-      src="/logo.png"
+      src="https://res.cloudinary.com/drdebnl11/image/upload/v1756872154/logo_ohculr.png"
       alt="Edora Logo"
       width={48}
       height={48}
